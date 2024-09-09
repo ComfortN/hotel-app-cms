fa1097f65548fe94287ee27023c54ff3ba815783
Comfort Ngwenya <>
2024-09-09 12:23:38 +0200

    Adding the edit and block function of the user

--- hotel-app-cms/src/components/UserManager.jsx
+++ hotel-app-cms/src/components/UserManager.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { Box, Card, CardContent, Typography, } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Card, CardContent, Typography, Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../redux/userSlice';
+import { fetchUsers, updateUser, blockUser } from '../redux/userSlice';
 
 
 export default function UserManager() {
@@ -10,11 +10,46 @@ export default function UserManager() {
     const userStatus = useSelector((state) => state.users.status);
     const error = useSelector((state) => state.users.error);
 
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [openEditDialog, setOpenEditDialog] = useState(false);
+    const [editUser, setEditUser] = useState({ firstName: '', lastName: '', email: '', phone: '', address: '' });
+
+
     useEffect(() => {
         if (userStatus === 'idle') {
             dispatch(fetchUsers());
         }
     }, [userStatus, dispatch]);
+
+
+    const handleOpenEditDialog = (user) => {
+        setSelectedUser(user);
+        setEditUser({
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            phone: user.phone,
+            address: user.address
+        });
+        setOpenEditDialog(true);
+    };
+
+    const handleCloseEditDialog = () => {
+        setOpenEditDialog(false);
+    };
+
+    const handleEditChange = (e) => {
+        setEditUser({ ...editUser, [e.target.name]: e.target.value });
+    };
+
+    const handleSaveEdit = () => {
+        dispatch(updateUser({ id: selectedUser.id, updates: editUser }));
+        handleCloseEditDialog();
+    };
+
+    const handleBlockUser = (userId) => {
+        dispatch(blockUser(userId));
+    };
 
     return (
         <Box>
@@ -24,12 +59,51 @@ export default function UserManager() {
             <Card key={user.id} sx={{ mb: 2 }}>
                 <CardContent>
                     <Typography variant="h6">User ID: {user.id}</Typography>
-                    <Typography>Name: {user.name}</Typography>
+                    <Typography>Name: {user.firstName} {user.lastName}</Typography>
                     <Typography>Email: {user.email}</Typography>
+                    <Typography>Phone: {user.phone}</Typography>
+                    <Typography>Address: {user.address}</Typography>
                     <Typography>Favorites: {user.favorites}</Typography>
+
+                    <Button variant="outlined" color="primary" onClick={() => handleOpenEditDialog(user)} sx={{ mr: 1 }}>
+                        Edit
+                    </Button>
+                    <Button variant="outlined" color="secondary" onClick={() => handleBlockUser(user.id)}>
+                        Block
+                    </Button>
                 </CardContent>
             </Card>
         ))}
+
+<Dialog open={openEditDialog} onClose={handleCloseEditDialog}>
+                <DialogTitle>Edit User</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        label="First Name" name="firstName" value={editUser.firstName}
+                        onChange={handleEditChange} fullWidth margin="normal"
+                    />
+                    <TextField
+                        label="Last Name" name="lastName" value={editUser.lastName} onChange={handleEditChange}
+                        fullWidth margin="normal"
+                    />
+                    <TextField
+                        label="Email" name="email" type="email" value={editUser.email} onChange={handleEditChange}
+                        fullWidth margin="normal"
+                    />
+                    <TextField
+                        label="Phone" name="phone" value={editUser.phone} onChange={handleEditChange}
+                        fullWidth margin="normal"
+                    />
+                    <TextField
+                        label="Address" name="address" value={editUser.address} onChange={handleEditChange}
+                        fullWidth margin="normal"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseEditDialog}>Cancel</Button>
+                    <Button onClick={handleSaveEdit} variant="contained" color="primary">Save</Button>
+                </DialogActions>
+            </Dialog>
     </Box>
     )
 }
--- hotel-app-cms/src/redux/userSlice.js
+++ hotel-app-cms/src/redux/userSlice.js
@@ -1,11 +1,26 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { database } from '../firebase/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 
 // Define async thunk for fetching users
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
     const querySnapshot = await getDocs(collection(database, 'users'));
     return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+});
+
+
+// Define async thunk for updating a user
+export const updateUser = createAsyncThunk('users/updateUser', async ({ id, updates }) => {
+    const userDocRef = doc(database, 'users', id);
+    await updateDoc(userDocRef, updates);
+    return { id, ...updates };
+});
+
+// Define async thunk for blocking a user
+export const blockUser = createAsyncThunk('users/blockUser', async (id) => {
+    const userDocRef = doc(database, 'users', id);
+    await updateDoc(userDocRef, { blocked: true });
+    return id;
 });
 
 // Define initial state
